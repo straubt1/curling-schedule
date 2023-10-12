1f057da2b0f6c5a64d1813e2acddb6e52373f679
straubt1 <tstraub@hashicorp.com>
2023-10-12 14:52:19 -0500

    test

--- scripts/teeline.sh
+++ scripts/teeline.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+get_next_date () {
+  local date_offset=$1  
+
+  case `uname` in
+
+  Darwin)
+    NEXT_DATE=$(date -v +${date_offset}d +%m-%d-%Y)
+    # TIMESTAMP=`date -v -${EXPIRY_DAYS}d +%Y-%m-%d`
+    ;;
+  Linux)
+    NEXT_DATE=$(date -u --date="+${date_offset}d" +%m-%d-%Y)
+    # TIMESTAMP=`date -u --date="-${EXPIRY_DAYS} day" +%Y-%m-%d`
+    ;;
+  *)
+    echo "Platform not supported. Exiting.";
+    exit 1;
+    ;;
+esac
+
+}
 get_bookings () {
   local date_offset=$1
 
@@ -7,17 +27,9 @@ get_bookings () {
   NEXT_DATE=$(date -v +${date_offset}d +%m-%d-%Y)
   
   curl -s "https://www.sevenrooms.com/api-yoa/availability/widget/range?venue=teeline&time_slot=17:00&party_size=4&halo_size_interval=24&start_date=${NEXT_DATE}&num_days=1&channel=SEVENROOMS_WIDGET&selected_lang_code=en" | jq -r '.data.availability | keys[] as $k | .[$k] | .[0].times[] | select(.type | contains("book")) | [(now | strflocaltime("%Y-%m-%d %H:%M:%S")), $k, .time] | @csv' >> "${NEXT_DATE}.csv"
-# "2023-10-10T18:10:39-0600"
 }
 
-# START_DATE="10-10-2023"
-# START_DATE=$(date +%m-%d-%Y)
-# date -v +0d +%m-%d-%Y
-# START_DATE=$(date -v +1d)
-# NUM_DAYS=1
-
-
-for i in {1..7}
+for i in {0..6}
 do
   get_bookings $i
 done
