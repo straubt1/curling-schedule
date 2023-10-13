0cf41321860feeac69a3f82005c4b01b5216ab9e
straubt1 <tstraub@hashicorp.com>
2023-10-12 19:38:08 -0500

    t

--- scripts/teeline.sh
+++ scripts/teeline.sh
@@ -37,6 +37,7 @@ get_bookings () {
   NEXT_FILENAME="dates/${NEXT_DATE}.csv"
   QUERY_DATE=$(TZ=":US/Central" date +"%m-%d-%Y %I:%M:%S %p")
   
+  echo "Next Date: ${NEXT_DATE}"
   create_date_file ${NEXT_FILENAME}
   # Get times that are open, returns a string with those times for a single day
   TIMES=`curl -s "https://www.sevenrooms.com/api-yoa/availability/widget/range?venue=teeline&time_slot=17:00&party_size=4&halo_size_interval=24&start_date=${NEXT_DATE}&num_days=1&channel=SEVENROOMS_WIDGET&selected_lang_code=en" | jq -r '.data.availability | keys[] as $k | .[$k] | .[0].times[] | select(.type == "book").time'`
