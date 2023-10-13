504c79378fb5ed43e4b455040575061d8b565b27
Tom Straub <tstraub@hashicorp.com>
2023-10-13 07:26:39 -0500

    Tt/updates (#2)
    * Update Cron
    
    ---------
    
    Co-authored-by: straubt1 <straubt1@users.noreply.github.com>

--- scripts/teeline.sh
+++ scripts/teeline.sh
@@ -6,11 +6,11 @@ get_next_date () {
   case `uname` in
 
   Darwin)
-    NEXT_DATE=$(date -v +${date_offset}d +%m-%d-%Y)
+    NEXT_DATE=$(TZ=":US/Central" date -v +${date_offset}d +%m-%d-%Y)
     # TIMESTAMP=`date -v -${EXPIRY_DAYS}d +%Y-%m-%d`
     ;;
   Linux)
-    NEXT_DATE=$(date -u --date="+${date_offset} day" +%m-%d-%Y)
+    NEXT_DATE=$(TZ=":US/Central" date -u --date="+${date_offset} day" +%m-%d-%Y)
     # TIMESTAMP=`date -u --date="-${EXPIRY_DAYS} day" +%Y-%m-%d`
     ;;
   *)
@@ -37,6 +37,7 @@ get_bookings () {
   NEXT_FILENAME="dates/${NEXT_DATE}.csv"
   QUERY_DATE=$(TZ=":US/Central" date +"%m-%d-%Y %I:%M:%S %p")
   
+  echo "Next Date: ${NEXT_DATE}"
   create_date_file ${NEXT_FILENAME}
   # Get times that are open, returns a string with those times for a single day
   TIMES=`curl -s "https://www.sevenrooms.com/api-yoa/availability/widget/range?venue=teeline&time_slot=17:00&party_size=4&halo_size_interval=24&start_date=${NEXT_DATE}&num_days=1&channel=SEVENROOMS_WIDGET&selected_lang_code=en" | jq -r '.data.availability | keys[] as $k | .[$k] | .[0].times[] | select(.type == "book").time'`
